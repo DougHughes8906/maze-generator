13335f9a1add0d2060672a428a548a4cf64ae3cb
Doug Hughes <dhughes8906@gmail.com>
2021-03-17 01:10:33 -0400

    Creates a stub for a render maze function
    The function will be used to take in a 2D grid of bool values and
    output a grid a characters representing the original grid, so the
    maze can be made visible either in the console or in a text file.

---
+++ renderMaze.cpp
@@ -0,0 +1,5 @@
+#include "renderMaze.h"
+
+char_grid_t renderMaze(const bool_grid_t &maze) {
+	return char_grid_t();
+}
---
+++ renderMaze.h
@@ -0,0 +1,11 @@
+#ifndef RENDER_MAZE_H
+#define RENDER_MAZE_H
+
+#include <vector>
+
+using char_grid_t = std::vector<std::vector<std::char>>;
+using bool_grid_t = std::vector<std::vector<bool>>; 
+
+char_grid_t renderMaze(const bool_grid_t &maze);
+
+#endif
