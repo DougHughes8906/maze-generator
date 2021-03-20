9f0f1672d6d67fd12b1eaaae6698972671cb7895
Doug Hughes <dhughes8906@gmail.com>
2021-03-20 02:27:05 -0400

    Alters a variable name for clarity
    Changes the name of the bool_val variable to is_wall to make
    clear the purpose of the variable.

--- renderMaze.cpp
+++ renderMaze.cpp
@@ -7,8 +7,8 @@ maze::char_grid_t renderMaze(const maze::bool_grid_t &maze) {
 
 	for (auto const &row : maze) {
 		std::vector<char> char_row;
-		for (auto bool_val : row) {
-			char_row.push_back((bool_val) ? maze::wall_chtr : maze::path_chtr);	
+		for (auto is_wall : row) {
+			char_row.push_back((is_wall) ? maze::wall_chtr : maze::path_chtr);	
 		}
 		char_maze.push_back(char_row);
 	}
