30366b5286147030a895fbebc755bd63af60c533
Doug Hughes <dhughes8906@gmail.com>
2021-03-20 02:22:34 -0400

    Implements the render maze function
    The function takes a grid of bool values and converts it to a
    grid of char values (to allow for direct printing / writing to
    a file).

--- renderMaze.cpp
+++ renderMaze.cpp
@@ -1,6 +1,17 @@
+#include <vector>
 #include "renderMaze.h"
 #include "maze.h"
 
 maze::char_grid_t renderMaze(const maze::bool_grid_t &maze) {
-	return maze::char_grid_t();
+	maze::char_grid_t char_maze;
+
+	for (auto const &row : maze) {
+		std::vector<char> char_row;
+		for (auto bool_val : row) {
+			char_row.push_back((bool_val) ? maze::wall_chtr : maze::path_chtr);	
+		}
+		char_maze.push_back(char_row);
+	}
+
+	return char_maze;
 }
