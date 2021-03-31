aa4017158db804e962d2cb12b2947857a1e86c71
Doug Hughes <dhughes8906@gmail.com>
2021-03-31 18:01:53 -0400

    Builds overall logic for buildSolutionPath
    Sets the overall logic for the buildSolutionPath function. The
    function does not yet perform expected behavior because numerous
    helper functions are only stubbed out.

--- generateMaze.cpp
+++ generateMaze.cpp
@@ -6,6 +6,19 @@
 #include "maze.h"
 #include "generateMaze.h"
 
+// information that is used related to a location to decide if it 
+// will be opened as path area
+struct ChoiceInfo {
+  std::pair<int, int> location;
+  bool available;
+  int probability;
+  ChoiceInfo(std::pair<int, int> loc, bool is_avail, int prob) {
+    location = loc;
+    available = is_avail;
+    probability = prob;
+  }
+};
+
 // randomly chooses the start location for the maze along one of the borders
 // of the maze. The corners of the maze are excluded as possible starting
 // locations.
@@ -115,11 +128,36 @@ std::pair<int, int> getAdjacent(std::pair<int, int> cur_loc,
   return next_loc;
 }
 
+// determines if path can be opened in a given location. It cannot be opened
+// if there will be two whitespaces next to it
+// PRECONDITION: the location is a valid location in the maze
+bool canOpenPath(std::pair<int, int> location, const maze::bool_grid_t &maze) {
+  return true;
+}
+
+// randomly chooses a location based on the probabilities passed in with
+// the ChoiceInfo structs. Only considers a choice if the location is 
+// available. 
+// If only one choice is available that location is returned.
+// If only one of right and left is available, the remaining choice gets
+//  the probability of both. 
+// If no choice is available, a location of (0, 0) is returned.
+std::pair<int, int> chooseNextLocation(const ChoiceInfo &forward_info,
+                                       const ChoiceInfo &right_info,
+                                       const ChoiceInfo &left_info,
+                                       std::mt19937 &random_engine) {
+  return forward_info.location;
+}
+
+bool isBorder(std::pair<int, int> location, const maze::bool_grid_t &maze) {
+  return true;
+} 
 
 // builds a single path from one border location to another that can be
 // used to solve the maze
 void buildSolutionPath(maze::bool_grid_t &maze, 
-                       std::pair<int, int> start_loc) {
+                       std::pair<int, int> start_loc,
+                       std::mt19937 &random_engine) {
   const int maze_side_len{ static_cast<int>(maze.size()) };
 
   // open the starting location
@@ -132,14 +170,44 @@ void buildSolutionPath(maze::bool_grid_t &maze,
   };
 
   // determine the next path location based on the starting 
-  // direction (one space forward from starting
-  auto next_loc{ getAdjacent(start_loc, start_direction, 
-                             maze::RelativeDirection::forward) }; 
+  // direction (one space forward from starting)
+  auto cur_loc{ getAdjacent(start_loc, start_direction, 
+                            maze::RelativeDirection::forward) }; 
 
   // open the next location
-  openLocation(next_loc, maze);  
+  openLocation(cur_loc, maze);  
+  auto cur_direction{ start_direction };
 
-  
+  // probabilities of moving in each direction
+  // the probability for any given direction is that value over the
+  // sum of all three values
+  int forward_probability{ 6 };
+  int right_probability{ 2 };
+  int left_probability{ 2 };
+
+  // flag used to determine when the solution path building is finished
+  bool finished{ false };
+  while (!finished) {
+    auto forward_loc{ getAdjacent(cur_loc, cur_direction,
+                                  maze::RelativeDirection::forward) }; 
+    auto right_loc{ getAdjacent(cur_loc, cur_direction,
+                                maze::RelativeDirection::right) }; 
+    auto left_loc{ getAdjacent(cur_loc, cur_direction,
+                               maze::RelativeDirection::left) }; 
+
+    bool forward_avail{ canOpenPath(forward_loc, maze) };
+    bool right_avail{ canOpenPath(right_loc, maze) };
+    bool left_avail{ canOpenPath(left_loc, maze) }; 
+
+    ChoiceInfo forward_info(forward_loc, forward_avail, forward_probability);
+    ChoiceInfo right_info(right_loc, right_avail, right_probability);
+    ChoiceInfo left_info(left_loc, left_avail, left_probability);
+    cur_loc = chooseNextLocation(forward_info, right_info, left_info,
+                                 random_engine);
+ 
+    openLocation(cur_loc, maze);
+    finished = isBorder(cur_loc, maze);
+  }  
 } 
 
 void buildBranches(maze::bool_grid_t &maze, std::pair<int, int> start_loc) {
@@ -157,7 +225,7 @@ maze::bool_grid_t generateMaze(int side_len, std::mt19937 &random_engine) {
 
   auto start_loc{ chooseStartLocation(side_len, random_engine) };
 
-  buildSolutionPath(new_maze, start_loc);
+  buildSolutionPath(new_maze, start_loc, random_engine);
 
   buildBranches(new_maze, start_loc);
 
