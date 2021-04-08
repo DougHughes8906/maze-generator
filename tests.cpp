7a5036512213213b12757f4b1e6aa5f0f49aef6d
Doug Hughes <dhughes8906@gmail.com>
2021-04-08 18:14:08 -0400

    Refactors tests to use findSolutionLength
    Previously used a less generalized function findPath that
    returned a boolean value.

--- tests.cpp
+++ tests.cpp
@@ -2,11 +2,11 @@
 #include <vector>
 #include <iostream>
 #include <random>
-#include <queue>
 #include "maze.h"
 #include "renderMaze.h"
 #include "generateMaze.h"
 #include "findStart.h"
+#include "findSolutionLength.h"
 
 /*
   Tests that two 2D vectors have the same number of rows, the 
@@ -65,105 +65,11 @@ void testRenderMaze() {
     test_fail_message);
 }
 
-// helper function to addOpenAdjacent
-// pushes a location into next_locs if it is "valid"
-// valid meaning it is a path area and is unexplored
-void pushIfValid(std::queue<std::pair<int, int>> &next_locs,
-                 const maze::bool_grid_t &maze, 
-                 const maze::bool_grid_t &explored,
-                 const std::pair<int, int> &location) {
-  if (!maze[location.first][location.second] &&
-      !explored[location.first][location.second]) {
-    next_locs.push(location);
-  } 
-}
-
-// helper function to findPath
-// Adds any valid open, unexplored path locations adjacent
-// to the given location to the next_locs queue
-void addOpenAdjacent(std::queue<std::pair<int, int>> &next_locs,
-                     const maze::bool_grid_t &maze, 
-                     const maze::bool_grid_t &explored,
-                     const std::pair<int, int> &location) {
-  int left_col{ location.second - 1 };
-  if (left_col >= 0) {
-    std::pair<int, int> left_loc(location.first, left_col);
-    pushIfValid(next_locs, maze, explored, left_loc);
-  }
-
-  int right_col{ location.second + 1 };
-  if (right_col < maze[location.first].size()) {
-    std::pair<int, int> right_loc(location.first, right_col);
-    pushIfValid(next_locs, maze, explored, right_loc); 
-  }
-
-  int row_above{ location.first - 1 };
-  if (row_above >= 0) {
-    std::pair<int, int> above_loc(row_above, location.second);
-    pushIfValid(next_locs, maze, explored, above_loc);
-  }
-
-  int row_below{ location.first + 1 };
-  if (row_below < maze.size()) {
-    std::pair<int, int> below_loc(row_below, location.second);
-    pushIfValid(next_locs, maze, explored, below_loc);
-  } 
-}       
-
-// returns true if the end of the maze can be reached
-// from the start and false otherwise.
-//
-// PRECONDITION: there is a single start location and
-// a single end location
-bool findPath(const maze::bool_grid_t &maze, 
-              const std::pair<int, int> &start_loc) {
-  // perform a breadth first search
-  
-  // mark another grid with explored locations
-  maze::bool_grid_t explored;
-  for (const auto &row : maze) {
-    explored.push_back(std::vector<bool>(row.size(), false));
-  }
-
-  explored[start_loc.first][start_loc.second] = true;
-
-  std::queue<std::pair<int, int>> next_locs;
-
-  addOpenAdjacent(next_locs, maze, explored, start_loc);
-
-  const int first_row{ 0 };
-  const int last_row{ static_cast<int>(maze.size() - 1) };
-  const int first_col{ 0 };
-  const int last_col{ static_cast<int>(maze[last_row].size() - 1) };
-
-  // perform the search
-  while(!next_locs.empty()) {
-    auto cur_loc{ next_locs.front() };
-    next_locs.pop();
-    explored[cur_loc.first][cur_loc.second] = true;
-
-    if (cur_loc.first == first_row || cur_loc.first == last_row ||
-        cur_loc.second == first_col || cur_loc.second == last_col) {
-      return true;
-    } 
-
-    addOpenAdjacent(next_locs, maze, explored, cur_loc);
-  }
-
-  return false;
-}
-
 // checks to see if a single maze is valid
-bool checkValid(const maze::bool_grid_t &maze) {
-  auto start_loc{ findStart(maze) };
-
-  if (start_loc.first != -1) {
-    return findPath(maze, start_loc);
-  }
-
-  return false;
+bool checkValid(const maze::bool_grid_t &maze) {  
+  int path_length{ findSolutionLength(maze) };
+  return (path_length != -1);
 }
-
 
 // tests that the generateMaze function generates "valid"
 // mazes. A maze is considered valid if it has a non-adjacent
